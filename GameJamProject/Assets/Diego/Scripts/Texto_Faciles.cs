5a891d3a9b1d1f72311f3bfb6c5f41e377faa38f
Folumero <costacostadiego@gmail.com>
2025-01-25 00:48:28 +0100

    SubidaCarpeta

---
+++ GameJamProject/Assets/Diego/Scripts/Texto_Faciles.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Texto_Faciles : MonoBehaviour
+{
+
+    private Sphere SphereScript;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
